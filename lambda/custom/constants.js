92347ac83e0ad0405c0b6a23883f1f8e0c5f70ff
Jonathan King <jking@fearless.tech>
2018-07-19 15:10:35 +0000

    TA-700 adds naics code validation

--- lambda/custom/constants.js
+++ lambda/custom/constants.js
@@ -9,6 +9,9 @@ module.exports = {
         errorMessage: "I'm sorry there was an error determining your business's status",
         positive: "Congratulations you qualify as a small business",
         negative: "I'm sorry this business does not qualify as a small business",
-        badEmployeeCount: "I didn't get that"
+        badEmployeeCount: "I'm sorry I didn't understand that.  Can you repeat how many employees you have?",
+        badReceipts: "I'm sorry, I didn't understand that.  Can you repeat your annual receipts?",
+        badNaicsCode: "I'm sorry, that is not a valid code.  Can you repeat your industry code?",
+        unableToHelp: "I'm sorry, but I can't help with that right now.  Try again later."
     }
 };
