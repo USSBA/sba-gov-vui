91b849b6ebe40083543ea5b7b93118f8f07207ac
jrwatts <jordan.r.watts@gmail.com>
2018-07-20 16:59:43 -0400

    makes messages more conversational

--- lambda/custom/constants.js
+++ lambda/custom/constants.js
@@ -3,23 +3,23 @@ module.exports = {
     sizeStandardsHostName: "mint.ussba.io"
   },
   ErrorIntent: {
-    message: 'Sorry, I can\'t understand the command. Please say again.'
+    message: 'Sorry, I didn\'t catch that. How can I help?'
   },
   CancelAndStopIntent: {
-    message: 'Hope we helped!'
+    message: 'Good luck with your business!'
   },
   LaunchRequestIntent: {
     welcomeText: 'Welcome to the S. B. A. Voice Assistant, how can I help?'
   },
   AmIASmallBusinessIntent: {
-    errorMessage: "I'm sorry there was an error determining your business's status",
-    positive: "Congratulations you qualify as a small business",
-    negative: "I'm sorry this business does not qualify as a small business",
-    badEmployeeCount: "I'm sorry I didn't understand that.  Can you repeat how many employees you have?",
-    badReceipts: "I'm sorry, I didn't understand that.  Can you repeat your annual receipts?",
-    badNaicsCode: "I'm sorry, that is not a valid code.  Can you repeat your industry code?",
-    badNaicsCodeNotAumber: "I'm sorry, that is not a valid code. A NAICS code should be a six digit number.  Can you repeat your industry code?",
-    unableToHelp: "I'm sorry, but I can't help with that right now.  Try again later."
+    errorMessage: "Sorry, I wasn't able to determine your business size. Could you try asking again?",
+    positive: "Congratulations, your business qualifies as small. You may be eligible to participate in government contracting programs and compete for contracts reserved or set aside for small businesses.",
+    negative: "I\'m sorry, this business does not qualify as a small business",
+    badEmployeeCount: "Sorry, I didn't get that. How many employees does your business employ?",
+    badReceipts: "Sorry, I didn't get that. What amount of annual receipts does your business have?",
+    badNaicsCode: "I\'m sorry, I don't recognize that naics code. Could you try a different naics code?",
+    badNaicsCodeNotAumber: "Sorry, I don't recognize that naics code. What is your six digit naics code?",
+    unableToHelp: "Sorry, but I can't help with that right now."
   },
   HelpIntent: {
     message: 'You can say : am I a small business!'
